a3b14f7f3c7b382335d96d6e262d52d393eb2a1a
Saurabh-Sonwane <saurabh@chat360.io>
2024-08-10 15:41:48 +0530

    c

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-# Install dependencies
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --no-input --clear
+python3.9 manage.py collectstatic --no-input --clear
